25cc9cb12a65131cc10b91362f10897f7359747e
Alan_John_Thomas <aajjrt1705@gmail.com>
2026-06-13 21:19:40 +0530

    created a repository for the book model

--- src/main/java/com/education/bookcatalog/repository/BookRepository.java
+++ src/main/java/com/education/bookcatalog/repository/BookRepository.java
@@ -1,5 +1,9 @@
 package com.education.bookcatalog.repository;
 
-public interface BookRepository {
+import com.education.bookcatalog.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface BookRepository extends JpaRepository<Book,Long> {
 }
