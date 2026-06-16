059f6db31fdc17f98684366382b389d2585e7495
Alan_John_Thomas <aajjrt1705@gmail.com>
2026-06-16 19:16:27 +0530

    added a post method to save a book in the database, added api-tests.http to test the endpoints directly

--- src/main/java/com/education/bookcatalog/controller/BookController.java
+++ src/main/java/com/education/bookcatalog/controller/BookController.java
@@ -3,20 +3,24 @@ package com.education.bookcatalog.controller;
 import com.education.bookcatalog.model.Book;
 import com.education.bookcatalog.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/books")
 @RequiredArgsConstructor
-
 public class BookController {
     private final BookRepository bookRepository;
+
     @GetMapping
     public List<Book> getAllBooks(){
         return bookRepository.findAll();
+    }
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Book createBook(@RequestBody Book book){
+        return bookRepository.save(book);
     }
 }
