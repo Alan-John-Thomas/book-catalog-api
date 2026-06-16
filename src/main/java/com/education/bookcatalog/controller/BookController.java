324bff8935c647ea76daaedcdf497688810691c5
Alan_John_Thomas <aajjrt1705@gmail.com>
2026-06-16 19:40:56 +0530

    added get endpoint to get a book by id

--- src/main/java/com/education/bookcatalog/controller/BookController.java
+++ src/main/java/com/education/bookcatalog/controller/BookController.java
@@ -5,8 +5,10 @@ import com.education.bookcatalog.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/books")
@@ -18,9 +20,16 @@ public class BookController {
     public List<Book> getAllBooks(){
         return bookRepository.findAll();
     }
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Book createBook(@RequestBody Book book){
         return bookRepository.save(book);
+    }
+
+    @GetMapping("/{id}")
+    public Book getBookById(@PathVariable Long id) {
+        return bookRepository.findById(id)
+        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Book not found"));
     }
 }
